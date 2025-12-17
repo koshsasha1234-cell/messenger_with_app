485e66d7c237d4d9864ff830147f2cc760e9b965
aalleekkssaannddeerr1122-cpu <aalleekkssaannddeerr1122@gmail.com>
2025-12-17 20:04:05 +0300

    fix: Separate server and client dependencies

--- .render-build.sh
+++ .render-build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install -r pc_app/requirements.txt
+pip install -r pc_app/server_requirements.txt
 
 # No database migrations needed for this simple schema, 
 # but if you had them (e.g., with Flask-Migrate), you would run them here:
